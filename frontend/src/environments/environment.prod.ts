5d59e40671343d54716d6c752018669f1a90df26
Aisha <aishapereyrasole@gmail.com>
2026-06-25 19:51:09 -0300

    :(

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://aisha-pereyra-sole-tp-2-prog-4-2026-c1-annr3bpbo.vercel.app',
-  uploadsUrl: 'https://aisha-pereyra-sole-tp-2-prog-4-2026.vercel.app',
+  uploadsUrl: 'https://aisha-pereyra-sole-tp-2-prog-4-2026-c1-annr3bpbo.vercel.app',
 };
