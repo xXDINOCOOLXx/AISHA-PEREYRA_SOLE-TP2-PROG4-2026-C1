1ed2f50472f983542ef6dbe493021c1b906cc0e6
Aisha <aishapereyrasole@gmail.com>
2026-06-25 18:33:26 -0300

    porfavor vercel

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://aisha-pereyra-sole-tp-2-prog-4-2026.vercel.app/api',
+  uploadsUrl: 'https://aisha-pereyra-sole-tp-2-prog-4-2026.vercel.app',
 };
